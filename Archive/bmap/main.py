985d66ff23b74917b7e53b9c4440dc1a0b9e3f22
Shan <gs0627@icloud.com>
2018-12-26 08:48:29 +0800

    20181223

--- Archive/bmap/main.py
+++
@@ -1,5 +0,0 @@
-sen = "abc,123,4,789,mnp"
-replace_word = ['abc','mnp']
-sen = sen.replace(replace_word, "gaoshan")
-
-string = re.sub("[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+', "",line)
