696c5c902e216e778ac68c0665eb593653c497cf
Shan <gs0627@icloud.com>
2020-08-16 21:18:37 +0800

    20200816 find city list

--- tcmap_spider.py
+++ tcmap_spider.py
@@ -1,25 +1,39 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
+import pandas as pd
+
 
 # if has Chinese, apply decode()
-html = urlopen("http://www.tcmap.com.cn/zhejiangsheng/").read().decode('gbk')
 
-soup = BeautifulSoup(html, features='html.parser')
-
-city_table_tag = soup.select("table strong a")
-# city_table_strong = city_table_tag.find_all('strong')
+def get_html(url):
+    html_text = urlopen(url).read().decode('gbk')
+    return html_text
 
 
-city_result = city_table_tag.select('a[class="blue"]')
+def get_city(html_text):
+    soup = BeautifulSoup(html_text, features='html.parser')
+    city_res = soup.select("table strong a")
+    for c in city_res:
+        print(c.get_text())
 
-# city = soup.find('table').find_all('strong')
-# <strong><a href=/zhejiangsheng/hangzhou.html  class=blue>杭州市</a></strong></td>
-# city_table = soup.find_all('a',href = re.compile(".+qu\.html"))
-# city_table_f8 = city_table.find_all('<strong><a href(.*?)</strong></td>')
-# city = soup.find_all('a', {"class": "blue"})
-# city = soup.select('page_left table tbody tr td a')
-# use class to narrow search
-city = soup.find('a', {"class": "blue"})
-# city = soup.find_all("a", {"src": re.compile('.*?\.jpg')})
-# document.querySelector("#page_left > table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(6) > a:nth-child(1)")
+
+
+
+# def city(url):
+#     html = urlopen(url).read().decode('gbk')
+#     soup = BeautifulSoup(html, features='html.parser')
+#     city_res = soup.select("table strong a")
+#     for city_list in city_res:
+#         print(city_list.get_text())
+#         return city_res
+
+
+if __name__ == '__main__':
+    # address = input("请输入你的地址：")
+    base_url = 'http://www.tcmap.com.cn'
+    zhejiang_sub_url = '/zhejiang/'
+    url = base_url + zhejiang_sub_url
+    html_text = get_html(url)
+    city_name = get_city(html_text)
+
