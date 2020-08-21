1f86135674bd0ac1988c6f047f86ba79e62c481d
Shan <gs0627@icloud.com>
2020-08-21 22:16:37 +0800

    20200821 find sub city list

--- tcmap_spider.py
+++ tcmap_spider.py
@@ -12,24 +12,23 @@ def get_html(url):
 def get_city(html_text):
     soup = BeautifulSoup(html_text, features='html.parser')
     city_res = soup.select('tr[bgcolor = "#f8f8f8"]') + soup.select('tr[bgcolor = "#ffffff"]')
-    city_table = pd.DataFrame(columns=('city', 'sub_city'))
+    base_url = 'http://www.tcmap.com.cn'
+    city_table = pd.DataFrame(columns=('city', 'sub_city', 'sub_city_url'))
     for city_n in city_res:
         city_n_1 = city_n.select('strong')
         city_n_2 = city_n.select('td > a')
         for city_sub_n in city_n_2:
-            print(city_n_1[0].string)
-            print(city_sub_n.string)
-            city_table = city_table.append({'city': city_n_1[0].string, 'sub_city': city_sub_n.string},
-                                           ignore_index=True)
+            sub_city_url = city_sub_n.get('href')
+            sub_city_url = base_url + sub_city_url
+            city_table = city_table.append(
+                {'city': city_n_1[0].string, 'sub_city': city_sub_n.string, 'sub_city_url': sub_city_url}
+                , ignore_index=True)
     return city_table
 
 
-def get_sub_city(html_text):
+def get_street(html_text):
     soup = BeautifulSoup(html_text, features='html.parser')
-    soup = soup.table
-    sub_city_res = soup.select("td > a")
-    for sub_c in sub_city_res:
-        print(sub_c.get_text())
+    street_res = soup.select('strong')
 
 
 if __name__ == '__main__':
@@ -38,6 +37,6 @@ if __name__ == '__main__':
     url = base_url + zhejiang_sub_url
     html_text = get_html(url)
     city_name = get_city(html_text)
-    print(city_name)
-    # sub_city_name = get_sub_city(html_text)
-    # print(city_name)
+    for sub_city_url in city_name[['sub_city_url']]:
+        street_info = get_street(sub_city_url)
+        print(street_info)
