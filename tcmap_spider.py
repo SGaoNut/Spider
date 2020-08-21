423454811750d23a7e0c029f935eedbb12a91ad6
Shan <gs0627@icloud.com>
2020-08-21 13:46:21 +0800

    20200821 find sub city list

--- tcmap_spider.py
+++ tcmap_spider.py
@@ -13,36 +13,20 @@ def get_html(url):
 
 def get_city(html_text):
     soup = BeautifulSoup(html_text, features='html.parser')
-    # city_res = soup.select("table tr")
     city_res_1 = soup.select('tr[bgcolor = "#f8f8f8"]')
     city_res_2 = soup.select('tr[bgcolor = "#ffffff"]')
     city_res = city_res_1 + city_res_2
-    # city_table = []
     city_table = pd.DataFrame(columns=('city', 'sub_city'))
     for city_n in city_res:
         city_n_1 = city_n.select('strong')
         city_n_2 = city_n.select('td > a')
-        # city_table.append(city_n_1[0].string)
-        # city_table.loc[0] = city_n_1[0].string
         for city_sub_n in city_n_2:
-            # city_sub_n_1 = city_sub_n.select('td > a')
             print(city_n_1[0].string)
             print(city_sub_n.string)
             city_table = city_table.append({'city': city_n_1[0].string, 'sub_city': city_sub_n.string}, ignore_index=True)
-            # city_table['city'].append(city_n_1[0].string)
-            # city_table['sub_city'].append(city_sub_n_1[0].string)
-            # city_table = city_table.append(pd.DataFrame(city_n_1[0].string)
-        # city_table = city_table.append(pd.DataFrame(city_n_1[0].string)
-        # print(city_n_1[0].string)
     return city_table
 
 
-
-
-
-    # city_res_0 = city_res[0]
-    # for c in city_res:
-    #     print(c.get_text())
 
 def get_sub_city(html_text):
     soup = BeautifulSoup(html_text, features='html.parser')
